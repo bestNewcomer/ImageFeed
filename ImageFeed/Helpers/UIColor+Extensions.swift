2d20ac4c451085a448fe6db0d71e3027422bf009
Rinat <NewcomerRus@yandex.ru>
2023-08-07 19:00:14 +0300

    Добавили цвета, моковые картинки, картинку на LaunchScreen

---
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extensions.swift
+//  ImageFeed
+//
+//  Created by Ринат Шарафутдинов on 07.08.2023.
+//
+
+import UIKit
+
+extension UIColor {
+        static var ypBlue: UIColor { UIColor(named: "YP Blue") ?? UIColor.green }
+        static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+        static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+        static var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+        static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+        static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
+        static var ypWhiteAlpha50: UIColor { UIColor(named: "YP White (Alpha50)") ?? UIColor.white}
+     }
