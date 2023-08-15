976d80c9b25ef903a61754b719477e6aa52783ec
Rinat <NewcomerRus@yandex.ru>
2023-08-15 15:04:12 +0300

    Добавили таблицу на основной экран приложения, добавили объекты на одну ячейку таблицы. Выставили констрейнты.

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    
+    
+}
+
---
+++ ImageFeed/ImagesList/ImagesListViewController.swift.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Ринат Шарафутдинов on 01.08.2023.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    
+    @IBOutlet private var tableView: UITableView!
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView( _ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell)
+        return imageListCell
+    }
+    
+    func configCell(for cell: ImagesListCell) {
+    }
+    
+    
+}
+
