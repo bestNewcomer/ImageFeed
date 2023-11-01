624d0f49489cf055f9a389768b91a62f0518968d
bestNewcomer <NewcomerRUS@yandex.ru>
2023-11-01 12:04:01 +0300

    Правка ошибки и отправка на ревью

--- ImageFeedUITests/ImageFeedUITests.swift
+++ ImageFeedUITests/ImageFeedUITests.swift
@@ -24,17 +24,19 @@ final class ImageFeedUITests: XCTestCase {
         let loginTextField = webView.descendants(matching: .textField).element
         XCTAssertTrue(loginTextField.waitForExistence(timeout: 5))
         loginTextField.tap()
-        loginTextField.typeText("newcomerrus@yandex.ru")
+        loginTextField.typeText("Указать свой логин")
         webView.swipeUp()
         let passwordTextField = webView.descendants(matching: .secureTextField).element
         XCTAssertTrue(passwordTextField.waitForExistence(timeout: 5))
         passwordTextField.tap()
-        passwordTextField.typeText("New100500")
+        passwordTextField.typeText("Указать свой пароль")
+        sleep(1)
+        app.toolbars.buttons["Done"].tap()
+        sleep(1)
         webView.swipeUp()
         webView.buttons["Login"].tap()
         let tablesQuery = app.tables
         let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
-        
         XCTAssertTrue(cell.waitForExistence(timeout: 5))
     }
     
